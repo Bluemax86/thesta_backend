55a89d2c011614bcbfb15ef7ad0fa7a630d05960
Bluemax86 <Bluemax86@yahoo.com>
2025-04-09 16:10:18 -0500

    change path to db

--- management_app/app.py
+++ management_app/app.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from flask import Flask, request, session, jsonify
 from flask_cors import CORS
